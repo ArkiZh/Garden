43301d2ded9d8a3da350d1fa05a4a4a126d64e1b
ArkiZh <youneverknowai@foxmail.com>
2018-05-22 22:14:46 +0800

    Making the home page.

--- garden-portal-parent/garden-portal-front/src/main/webapp/static/js/index.js
+++ garden-portal-parent/garden-portal-front/src/main/webapp/static/js/index.js
@@ -1,14 +1,27 @@
 $(function () {
+
+    //获取首页欢迎图片的canvas对象
     var canvas = document.getElementById("homeCanvas");
-    var ClientWidth = window.screen.availWidth;
-    var ClientHeight = window.screen.availHeight;
-    canvas.setAttribute("width", ClientWidth);
-    canvas.setAttribute("height", ClientHeight);
+    //设置canvas区域大小
+    var ClientWidth = document.body.clientWidth;
+    var welcomeImageWidth = ClientWidth;
+    var wecomeImageHeight = welcomeImageWidth*0.45;
+    canvas.setAttribute("width", welcomeImageWidth.toString());
+    canvas.setAttribute("height", wecomeImageHeight.toString());
     var pen = canvas.getContext("2d");
-    pen.moveTo(0, 0);
-    pen.lineTo(ClientWidth/2, ClientHeight/2);
-    pen.lineTo(ClientWidth, 0);
-    pen.lineWidth=1;
-    pen.strokeStyle="#FF0000";
-    pen.stroke();
+    //加载欢迎图片
+    var welcomeImage = new Image();
+    welcomeImage.src = "static/image/元旦壁纸.jpg";
+    welcomeImage.onload=function(){
+        pen.fillStyle = pen.createPattern(welcomeImage, "no-repeat");
+        //先按图片原大小填充，再缩放为canvas区域大小
+        pen.rect(0,0,welcomeImage.naturalWidth,welcomeImage.naturalHeight);
+        pen.scale(welcomeImageWidth/welcomeImage.naturalWidth,wecomeImageHeight/welcomeImage.naturalHeight);
+        //填充canvas
+        pen.fill();
+    };
+    function alertMessage(){alert("Hello!");}
+    BindClickEventToRectangle({rectArea:{x:0,y:0,width:welcomeImageWidth,height:wecomeImageHeight},clickEvent:alertMessage});
+
 });
+
---
+++ garden-portal-parent/garden-portal-front/src/main/webapp/static/js/util/util.js
@@ -0,0 +1,32 @@
+/**
+ * 为指定区域绑定点击事件
+ * @param option 示例值：{rectArea:{x:0,y:0,width:30,height:30},clickEvent:alertMessage}
+ * @constructor
+ */
+var BindClickEventToRectangle=function (option) {
+    this.rectArea=option.rectArea;
+    this.clickEvent=option.clickEvent;
+
+    var judgeInsideRectArea=function(x,y,rectArea){
+        return x>rectArea.x && x<(rectArea.x+rectArea.width)
+            && y>rectArea.y && y<(rectArea.y+rectArea.height);
+    }
+    this.trigger={};
+    this.trigger.list=[];
+    if(option) this.trigger.list.push(option);
+
+    this.trigger.listen = function () {
+        //访问的时this的list，this指trigger
+        var list = this.list;
+        document.addEventListener('click', function (event) {
+            for (var i = 0; i < list.length; ++i) {
+                var bindedEvent = list[i];
+                if(judgeInsideRectArea(event.clientX, event.clientY, bindedEvent.rectArea)) {
+                    bindedEvent.clickEvent(event);
+                }
+            }
+        });
+    };
+
+    this.trigger.listen();
+};
